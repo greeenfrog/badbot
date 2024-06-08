d932fd06484490168a95418a27cd05a9649fa77c
jianing <lijn2003@protonmail.com>
2024-06-08 22:12:36 +1200

    Base configuration file

---
+++ base_config.py
@@ -0,0 +1,10 @@
+# Base configuration file - rename to "config.py"
+
+# API
+page_id = 'FACEBOOK PAGE ID'
+page_access_token = 'FACEBOOK PAGE ACCESS TOKEN'
+igsid = 'INSTAGRAM ID'
+
+# Driver
+username = 'INSTAGRAM USERNAME'
+password = 'INSTAGRAM PASSWORD'
